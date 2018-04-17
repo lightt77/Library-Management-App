4542a0e4bddee96a5db595053e7a13c2b30c26ad
lightt77 <abhisheksatam17@gmail.com>
2018-04-17 17:06:01 +0530

    Added new stored procs

--- sqlscripts/stored_procs.sql
+++ sqlscripts/stored_procs.sql
@@ -39,3 +39,32 @@ AS
 	SELECT * from dbo.title WHERE author=@author_name;
 GO
 EXEC dbo.GetBooksByAuthor @author_name ='xyz';
+
+--------------------------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE dbo.AddTitle (@title_name varchar(50),@author_name varchar(50),@rating int,@quantity int,@price int)
+AS
+	INSERT INTO dbo.title(title_name,author,rating,quantity,price,created_on,last_updated)
+		VALUES (@title_name,@author_name,@rating,@quantity,@price,SYSDATETIME(),SYSDATETIME());
+GO
+EXEC dbo.AddTitle @author_name ='xyz', @title_name='abhsdijid', @rating=5, @quantity=100, @price=100 ;
+
+--------------------------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE dbo.DeleteTitle (@title_name varchar(50),@author_name varchar(50),@rating int,@quantity int,@price int)
+AS
+	INSERT INTO dbo.title(title_name,author,rating,quantity,price,created_on,last_updated)
+		VALUES (@title_name,@author_name,@rating,@quantity,@price,SYSDATETIME(),SYSDATETIME());
+GO
+
+EXEC dbo.AddTitle @author_name ='xyz', @title_name='abhsdijid', @rating=5, @quantity=100, @price=100 ;
+
+
+--------------------------------------------------------------------------------------------
+--incomplete
+CREATE OR ALTER PROCEDURE dbo.GetBooksCurrentlySubcscribedToGivenUser (@user_id int)
+AS
+	--SELECT * from dbo.title WHERE author=@author_name;
+	SELECT title_name from dbo.title join dbo.book on dbo.book.title_id=dbo.title.title_id join dbo.rental on dbo.book.book_id=dbo.rental.book_id where dbo.rental.user_id=@user_id AND dbo.rental.rental_status=0;
+GO
+EXEC dbo.GetBooksCurrentlySubcscribedToGivenUser @user_id = 1;
