4bf65bbdce236a39119036ab19f3bb82e03564f1
lightt77 <abhisheksatam17@gmail.com>
2018-05-03 14:51:07 +0530

    improved filter logic

--- ui/scripts/catalogue/CatalogueController.js
+++ ui/scripts/catalogue/CatalogueController.js
@@ -1,34 +1,43 @@
 catalogueModule.controller('CatalogueController', ['$scope', 'CatalogueService', function ($scope, CatalogueService) {
 
-    $scope.search={
-         Title:"",
-         Author:"",
-         Genre:""
+    $scope.searchBookBy = 'Title';
+    $scope.search = {
+        Title: "",
+        Author: "",
+        Genre: ""
     };
 
-    (function () {
-        CatalogueService.getAllBooks().then((response) => {
-            $scope.booksList = response.data;
+    $scope.clearSearchFields = function () {
+        switch ($scope.searchBookBy) {
+            case 'Title': $scope.search.Author = ''; $scope.search.Genre = ''; break;
+            case 'Author': $scope.search.Title = ''; $scope.search.Genre = ''; break;
+            case 'Genre': $scope.search.Author = ''; $scope.search.Title = ''; break;
+            default: $scope.search.Title = $scope.searchBookInput; break;
+        }
+    };
 
-            console.log($scope.booksList);
-        }, (error) => {
-            console.log(error);
-        });
-    })();
+        (function () {
+            CatalogueService.getAllBooks().then((response) => {
+                $scope.booksList = response.data;
+
+                console.log($scope.booksList);
+            }, (error) => {
+                console.log(error);
+            });
+        })();
 
     $scope.setSearchBy = function () {
         console.log("setSearchBy called");
 
         switch ($scope.searchBookBy) {
-            case 'Title': console.log("jkd");$scope.search.Title = $scope.searchBookInput; break;
+            case 'Title': console.log("jkd"); $scope.search.Title = $scope.searchBookInput; break;
             case 'Author': $scope.search.Author = $scope.searchBookInput; break;
             case 'Genre': $scope.search.Genre = $scope.searchBookInput; break;
             default: $scope.search.Title = $scope.searchBookInput; break;
         }
     };
 
-    $scope.foo=function()
-    {
+    $scope.foo = function () {
         console.log("jaldkjlakjdljaskdjalksjd");
     };
 
