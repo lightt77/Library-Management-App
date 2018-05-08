23a406136c7d88a71e919febedf3e6cf0c12c5f2
lightt77 <abhisheksatam17@gmail.com>
2018-05-08 08:49:18 +0530

    Tentative login without session-based authentication

--- ui/scripts/login/LoginController.js
+++ ui/scripts/login/LoginController.js
@@ -1,4 +1,4 @@
-loginModule.controller('LoginController', ['$scope', 'LoginService', function ($scope, LoginService) {
+loginModule.controller('LoginController', ['$scope', 'LoginService', '$window', function ($scope, LoginService,$window) {
 
     $scope.login = function () {
         //console.log($scope.loginDetails);
@@ -6,6 +6,8 @@ loginModule.controller('LoginController', ['$scope', 'LoginService', function ($
             (response) => {
                 console.log("success");
                 console.log(response);
+                // redirect to catalogue page on successful login
+                $window.location.href = '#!/home/catalogue';
             },
             (error) => {
                 console.log("error");
--- webapi/webapi/Controllers/AccountController.cs
+++ webapi/webapi/Controllers/AccountController.cs
@@ -3,14 +3,19 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using webapi.Models;
 using webapi.Services;
+using System.Web;
+using System.Net.Http.Headers;
+using System;
+using System.Web.SessionState;
 
 namespace webapi.Controllers
 {
-    public class AccountController : ApiController
+    public class AccountController : ApiController, IRequiresSessionState, IReadOnlySessionState
     {
         private AccountService accountService = new AccountService();
 
         [EnableCors(origins: "http://127.0.0.1:5500", headers: "*", methods: "*")]
+        //[WebMethod(EnableSession=true)]
         [Route("users/login")]
         [HttpPost]
         public HttpResponseMessage Login([FromBody]Users user)
@@ -26,16 +31,37 @@ namespace webapi.Controllers
 
             if (accountService.ValidateLogin(user) == true)
             {
-                return new HttpResponseMessage()
-                {
-                    StatusCode = System.Net.HttpStatusCode.Accepted
-                };
+                // save the user in session
+                //session[user.EmailAddress] = user;
+                //if (HttpContext.Current.Session == null)
+                //    throw new Exception("Session is null");
+
+                //HttpContext.Current.Session["email"] = user;
+
+                // add a cookie to the response with email address of the user
+                //var cookie = new CookieHeaderValue("session-id", HttpContext.Current.Session.SessionID);
+                var cookie = new CookieHeaderValue("logged-in", "true");
+                cookie.Expires = DateTimeOffset.Now.AddDays(1);
+                cookie.Domain = Request.RequestUri.Host;
+                cookie.Path = "/";
+
+                var response = new HttpResponseMessage();
+
+                response.Headers.AddCookies(new CookieHeaderValue[] { cookie });
+                response.StatusCode = System.Net.HttpStatusCode.Accepted;
+
+                return response;
             }
 
             return new HttpResponseMessage()
             {
                 StatusCode = System.Net.HttpStatusCode.Unauthorized
             };
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
         }
 
         [EnableCors(origins: "http://127.0.0.1:5500", headers: "*", methods: "*")]
@@ -48,6 +74,6 @@ namespace webapi.Controllers
                 StatusCode = accountService.Register(user)
             };
         }
-        
+
     }
 }
